29fac49697f4efee2acc41884c6f1fc326621d84
leesiri1004 <leesiri1004@gmail.com>
2021-02-08 17:39:48 +0900

    웹페이지 구현

---
+++ Kmarket/js/scrolltop.js
@@ -0,0 +1,9 @@
+
+        $(function() {
+            $("#top").click(function() {
+                $('html, body').animate({
+                    scrollTop : 0
+                }, 400);
+                return false;
+            });
+        });
